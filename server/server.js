fa0bcc58fa737a8c31351c11f9ffd7ccd69bf839
Kobus Viljoen <kobusv@gmail.com>
2017-06-13 18:39:11 +0200

    added error and users routes with tests.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 let app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+    res.send([{
+        name: 'Sarel',
+        age: 17
+    }, {
+        name: 'Kobus',
+        age: 30
+    }, {
+        name: 'Pieter',
+        age: 22
+    }])
+});
+
 app.get('/error', (req, res) => {
     res.status(404).send({
         error: 'Page not found.'
--- server/server.test.js
+++ server/server.test.js
@@ -1,18 +1,47 @@
 const request = require('supertest');
 const expect = require('expect');
+const { describe, it } = require('mocha');
 
 const { app } = require('./server');
 
-describe('GET /error', () => {
-    it('Should return error response', (done) => {
-        request(app)
-            .get('/error')
-            .expect(404)
-            .expect((res) => {
-                expect(res.body).toInclude({
-                    error: 'Page not found.'
+describe('Server', () => {
+    describe('GET /', () => {
+        it('Should return Hello World!', (done) => {
+            request(app)
+                .get('/')
+                .expect(200)
+                .expect('Hello World!')
+                .end(done)
+        });
+    });
+
+    describe('GET /error', () => {
+        it('Should return error response', (done) => {
+            request(app)
+                .get('/error')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    })
                 })
-            })
-            .end(done)
+                .end(done)
+        });
+    });
+
+    describe('GET /users', () => {
+        it('Should return a list of users', (done) => {
+            request(app).get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body)
+                        .toBeAn('array')
+                        .toInclude({
+                            name: 'Kobus',
+                            age: 30
+                        })
+                })
+                .end(done)
+        })
     });
 });
